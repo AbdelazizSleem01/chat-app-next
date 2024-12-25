f608f4b86e02eab8e8c1be87cb1f49a134a44554
AbdelazizSleem01 <abdelazizsleem01@gmail.com>
2024-12-25 23:22:49 +0200

    fix import error

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true, 
+    experimental: {
+      appDir: true, 
+    },
     async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/conversations',
-                permanent: true,
-            },
-        ];
-    }
-};
-
-export default nextConfig;
+      return [
+        {
+          source: '/',
+          destination: '/conversations',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
